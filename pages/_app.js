7ebf2350051cb2019c5ccd95af0bf0476f4df043
Rohit Kumar <rohitkumar10701@gmail.com>
2023-02-10 20:41:52 +0530

    till search bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import '@/styles/globals.css'
+import '@/styles/index.css'
 import {SessionProvider} from 'next-auth/react'
 export default function App({ Component, pageProps,session }) {
   return (
